57436711cff2ca7cd895d17422c7a5b70d17a8fb
Nahuel <nahucastle98@gmail.com>
2022-05-05 23:49:21 -0300

    ingreso

--- superior.php
+++ superior.php
@@ -21,13 +21,13 @@ if(isset($_SESSION['nombredelusuario']))
 }
 else
 {
-  header('location: index.php');
+  header('location: login.php');
 }
 
 if(isset($_POST['btncerrar']))
 {
   session_destroy();
-  header('location: index.php');
+  header('location: login.php');
   $query=mysqli_query($conn,"UPDATE login SET access = 0 where usuario = '".$nom."' and password = '".$pass."'");
 
 }
