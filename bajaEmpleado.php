fa5daf7bb101ad4b52d2d90f74a305694331503c
LiluRi <lilenriquelme@gmail.com>
2022-05-30 18:00:29 -0300

    conexion bd baja empleado

--- bajaEmpleado.php
+++ bajaEmpleado.php
@@ -1,13 +1,19 @@
 <?php require_once "superior.php"?>
 <?php require_once "costado.php"?>
 <?php require_once "session.php" ?>
+
+
+<?php
+$conexion=mysqli_connect('localhost','root','','users')
+?>
 <main class="main col">
    <div class="container">
       <table class="table col-12">
          <h1 class="encabezado">Lista de Empleados</h1>
             <thead>
                <tr>
-                  <th scope="col">Nombre y Apellido</th>      
+                  <th scope="col">Nombre</th>    
+                  <th scope="col">Apellido</th>     
                   <th scope="col">Fecha de Alta</th>
                   <th scope="col">Télefono</th>
                   <th scope="col">Mail</th>
@@ -15,38 +21,27 @@
                   <th scope="col">Eliminar</th>         
                </tr>         
             </thead>
+            <?php
+            $sql="SELECT * from login";
+            $result= mysqli_query($conexion,$sql);
+            while($mostrar=mysqli_fetch_array($result)){
+               ?>
             <tbody>
                <tr>
-                  <th scope="row" data-label="Nombre y Apellido">francisco</th>
-                  <td data-label="Fecha de alta">6/5/22</td>
-                  <td data-label="Telefono">1133485004</td>
-                  <td data-label="Mail">fs080700@gmail.com</td>
-                  <td data-label="Usuario">Fran</td>               
+                  <th data-label="Nombre"> <?php echo $mostrar['nombre']?></th>
+                  <th data-label="Apellido"> <?php echo $mostrar['apellido']?></th>
+                  <td data-label="Fecha de alta"><?php echo $mostrar['fechaalta'] ?></td>
+                  <td data-label="Telefono"><?php echo $mostrar['telefono'] ?></td>
+                  <td data-label="Mail"><?php echo $mostrar['mail'] ?></td>
+                  <td data-label="Usuario"><?php echo $mostrar['usuario'] ?></td>               
                   <td data-label="Eliminar">
                      <button type="button" class="btn btn-danger"><i class="fa-regular fa-trash-can"></i></button>
                   </td>
-               </tr>
-               <tr>
-                  <th scope="row" data-label="Nombre y Apellido">Lilen</th>
-                  <td data-label="Fecha de alta">6/5/22</td>
-                  <td data-label="Telefono">1133485004</td>
-                  <td data-label="Mail">fs080700@gmail.com</td>
-                  <td data-label="Usuario">Lilen</td>
-                  <td data-label="Eliminar">
-                      <button type="button" class="btn btn-danger"><i class="fa-regular fa-trash-can"></i></button>
-                  </td>
-               </tr>
-               <tr>
-                  <th scope="row" data-label="Nombre y Apellido">Nahuel</th>
-                  <td data-label="Fecha de Alta">6/5/22</td>
-                  <td data-label="Telefono">1133485004</td>
-                  <td data-label="Mail">fs080700@gmail.com</td>
-                  <td data-label="Usuario">Nahuel</td>
-                  <td data-label="Eliminar">
-                     <button type="button" class="btn btn-danger"><i class="fa-regular fa-trash-can"></i></button>
-                   </td>
                </tr>
             </tbody>
+            <?php
+            }
+            ?>
       </table>
    </div>
 </main>
