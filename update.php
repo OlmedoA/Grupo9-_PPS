d135a45864d394c2e1ab254bb75466e54596f9f8
OlmedoA <axelolmedo1@gmail.com>
2022-06-12 18:52:20 -0300

    update

--- update.php
+++ update.php
@@ -40,18 +40,25 @@
                </div>
 
                <?php 
+			   //$cont=0;
                $query = "SELECT * FROM cerrados WHERE titulo = '$titulo'";
                $result = mysqli_query($conn, $query);
                while ($data=mysqli_fetch_object($result)){
                   $servicio=$data->servicio;
                   $cantidad=$data->cantidad;
+                  $cod=$data->cod;
+				  //$cont=$cont + 1;
+				  //$dato="servicio".$cont."";
                ?>
                <div class="col-12">
                <!--servicio-->
                <label for="servicio" class="form-label">Servicio</label>
                <select id="inputState" class="form-select form-control" name="servicio">
+
                <option selected><?php echo $servicio; ?></option>
                <?php
+			   
+			 //$dato2="cantidad".$cont."";
                $conn = mysqli_connect($servername, $username, $password, $database);
                $queryservicios= mysqli_query($conn,"SELECT * FROM `servicios`");
                $nr = mysqli_num_rows($queryservicios);
@@ -59,12 +66,16 @@
                   $opcion=$I['Descrip'];
                   echo"<option>$opcion</option>";
                   }
-               ?>
+                ?>
                </select>
                </div>
                <div class="col-12">
                   <label for="celular" class="form-label">Cantidad</label>
                   <input type="tel" class="form-control"  id="cantidad" name="cantidad" value="<?php echo $cantidad; ?>">
+                  <br>
+               </div>
+			   <div class="col-12">
+                  <input type="tel" class="form-control" style="display: none;"  id="cod" name="cod" value="<?php echo $cod; ?>">
                   <br>
                </div>
                <div class="col-12" align="center">
@@ -76,6 +87,7 @@
                ?>
             </div>
          <div class="modal-footer">
+            <button class="btn btn-primary" type="submit" name="cambiartodos">cambiar todos</button>
             <a href="PresupuestosPendientes.php"><button type="button" class="btn btn-secondary" data-dismiss="modal">Volver</button></a>
          </div>
       </form>
@@ -93,13 +105,13 @@
       if(!$conn){
          die("No hay conexión: ".mysqli_connect_error());
       }
-
+	  $codi=$_POST['cod'];
       $titulo = $_POST['title'];
       $celular = $_POST['celular'];
       $servicio = $_POST['servicio'];
       $cantidad = $_POST['cantidad'];
       $subtotal = 0.00;
-      $cons = mysqli_query($conn,"INSERT INTO `pretemp` (titulo, cel_cliente, servicio, cantidad, subtotal) VALUES ('$titulo', '$celular', '$servicio', '$cantidad', '$subtotal')");
+      $cons = mysqli_query($conn,"INSERT INTO `pretemp` (id, titulo, cel_cliente, servicio, cantidad, subtotal) VALUES ('$codi','$titulo', '$celular', '$servicio', '$cantidad', '$subtotal')");
 
       $query = "SELECT `Precio` FROM `servicios` WHERE `Descrip` = '$servicio'";
       $result = mysqli_query($conn, $query);           
@@ -107,7 +119,6 @@
          $precio=$row->Precio;
          $query1 = mysqli_query($conn,"UPDATE `pretemp` SET `subtotal` = '$precio' * '$cantidad'  WHERE `servicio` = '$servicio'");
       }
-	  $refe=$_GET['titulo'];
 	  $res = mysqli_query($conn, "SELECT * FROM pretemp");           
         while ($table=mysqli_fetch_object($res)){
 			
@@ -120,7 +131,7 @@
             $fecha=date("Y/m/d");
             $por=$_SESSION['nombredelusuario'];
             $unitario = 0;
-			 $query = mysqli_query($conn,"UPDATE `cerrados` SET `titulo`='$titulo',`cel_cliente`='$celular',`servicio`='$servicio',`cantidad`='$cantidad',`unitario`='$unitario',`subtotal`='$subtotal',`estado`='Pendiente',`fecha`='$fecha',`creado_por`='$por' WHERE `titulo`='$refe'");
+			 $query = mysqli_query($conn,"UPDATE `cerrados` SET `titulo`='$titulo',`cel_cliente`='$celular',`servicio`='$servicio',`cantidad`='$cantidad',`unitario`='$unitario',`subtotal`='$subtotal',`estado`='Pendiente',`fecha`='$fecha',`creado_por`='$por' WHERE `cod`='$codi'");
 
             $sql = "SELECT `Precio` FROM `servicios` WHERE `Descrip` = '$servicio'";
             $response = mysqli_query($conn, $sql);           
@@ -135,7 +146,66 @@
 
       
 }
+/*/
+if(isset($_POST['cambiartodos'])){ 
+      //conexion con la base de datos
+      $servername = "localhost";
+      $database = "jacesi";
+      $username = "root";
+      $password = "";
+      // crear conexion
+      $conn = mysqli_connect($servername, $username, $password, $database);
+      if(!$conn){
+         die("No hay conexión: ".mysqli_connect_error());
+      }
+	  $tit=$_GET['titulo'];
+	  $query = "SELECT * FROM cerrados WHERE titulo = '$tit'";
+      $result = mysqli_query($conn, $query);
+	  $conta=0;
+	  while ($data=mysqli_fetch_object($result)){
+	  $refes=$data->servicio;
+	  $conta=$conta + 1;
+	  $data1="servicio".$conta."";
+	  $data2="cantidad".$conta."";
+      $titulo = $_POST['title'];
+      $celular = $_POST['celular'];
+      $servicio = $_POST["".$data1.""];
+      $cantidad = $_POST["".$data2.""];
+      $subtotal = 0.00;
+      $cons = mysqli_query($conn,"INSERT INTO `pretemp` (titulo, cel_cliente, servicio, cantidad, subtotal) VALUES ('$titulo', '$celular', '$servicio', '$cantidad', '$subtotal')");
 
+      $query = "SELECT `Precio` FROM `servicios` WHERE `Descrip` = '$servicio'";
+      $result = mysqli_query($conn, $query);           
+      while ($row=mysqli_fetch_object($result)){
+         $precio=$row->Precio;
+         $query1 = mysqli_query($conn,"UPDATE `pretemp` SET `subtotal` = '$precio' * '$cantidad'  WHERE `servicio` = '$servicio'");
+      }
+	  $res = mysqli_query($conn, "SELECT * FROM pretemp");           
+        while ($table=mysqli_fetch_object($res)){
+			
+			
+            $titulo=$table->titulo;
+            $celular=$table->cel_cliente;
+            $servicio=$table->servicio;
+            $cantidad=$table->cantidad;
+            $subtotal=$table->subtotal;
+            $fecha=date("Y/m/d");
+            $por=$_SESSION['nombredelusuario'];
+            $unitario = 0;
+			 $query = mysqli_query($conn,"UPDATE `cerrados` SET `titulo`='$titulo',`cel_cliente`='$celular',`servicio`='$servicio',`cantidad`='$cantidad',`unitario`='$unitario',`subtotal`='$subtotal',`estado`='Pendiente',`fecha`='$fecha',`creado_por`='$por' WHERE `servicio`='$refes'");
+
+            $sql = "SELECT `Precio` FROM `servicios` WHERE `Descrip` = '$servicio'";
+            $response = mysqli_query($conn, $sql);           
+            while ($row=mysqli_fetch_object($response)){
+                $precio=$row->Precio;
+                $cons = mysqli_query($conn, "UPDATE `cerrados` SET `unitario` = '$precio'  WHERE `servicio` = '$servicio'");
+            }
+        }
+	  }
+		$consulta = mysqli_query($conn,  "TRUNCATE TABLE pretemp");
+		//echo "<script>window.location= 'PresupuestosPendientes.php' </script>";
+	}/*/
+	
 ?>
 
 <?php require_once "inferior.php"?>
