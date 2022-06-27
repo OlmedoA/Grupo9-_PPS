c217ce683fb10ebe7f9b227ea28c16c676d00b87
LiluRi <lilenriquelme@gmail.com>
2022-06-27 03:37:51 -0300

    conversor

--- altaServicios.php
+++ altaServicios.php
@@ -5,8 +5,11 @@
     <main class="main col">
         <!--Formulario alta de usuarios-->
         <form action="altaServiciosSql.php"  method="POST" class="row g-3 formStyle mx-auto py-4 px-4 form">
-                    <div class="form">
+                    <div class="form" id="app">
                         <h1 class="encabezado">Alta de servicio</h1>
+                     <div class="col-12 conversor" style="float: left; text-align: left;">
+                        <label><b> 1 USD$ = {{ calcularResultado }} ARS$ </b></label>
+                    </div>    
                     <div class="col-12">
                         <!--servicio, type text -->
                         <label for="Descrip" class="form-label">Servicio</label>
@@ -17,6 +20,8 @@
                         <label for="Precio" class="form-label">Precio</label>
                         <input type="number" class="form-control" id="Precio" name="Precio" required="required"><br>
                     </div>
+                   
+
 
                         <!--boton-->
                     <button class="btn btn-primary" type="submit">Crear Servicio</button>
